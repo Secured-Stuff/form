28da7e469f9f954845389676f4d333eca24debe0
Adrian Bieniek <kontakt@abieniek.dev>
2024-05-23 00:22:51 +0200

    Dockerize app

--- app/api/route.ts
+++ app/api/route.ts
@@ -4,7 +4,19 @@ import puppeteer from "puppeteer";
 import { invoiceTemplate } from "../_assets/invoiceTemplate";
 
 async function generatePDF(htmlContent: any) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+
   const page = await browser.newPage();
   await page.setContent(htmlContent);
   const pdfBuffer = await page.pdf({ format: "A4" });
